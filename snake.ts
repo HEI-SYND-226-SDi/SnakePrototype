e3c329a6d66529cd3504e4a31bda014e5b5e8ba6
Michael Clausen <michael.clausen@hevs.ch>
2020-04-08 08:20:35 +0200

    Added SnakeGame class without any method implementation

--- snake.ts
+++ snake.ts
@@ -14,3 +14,25 @@ enum Direction {
     LEFT,
     RIGHT
 }
+
+class SnakeGame {
+    private display = $('#display')[0] as HTMLCanvasElement;
+    private direction = Direction.RIGHT;
+    private snake = [
+        new Point(32, 16),
+        new Point(31, 16),
+        new Point(30, 16),
+        new Point(30, 15),
+        new Point(30, 14),
+        new Point(30, 13),
+        new Point(29, 13)
+    ];
+
+    private update() {}
+
+    private render() {}
+}
+
+$(() => {
+    const game = new SnakeGame();
+});
